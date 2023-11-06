ffadaeeb0bd35018ed65aae02f1a276bf75d9c73
DariaGolovacheva <144705786+DariaGolovacheva@users.noreply.github.com>
2023-11-06 15:00:22 +0300

    Update calculator.js

--- 6/calculator.js
+++ 6/calculator.js
@@ -80,6 +80,6 @@ function calculate_price_items()
     }
     else
     {
-        document.getElementById("result_items").textContent = "Неправильный формат ввода кол-ва товара";
+        document.getElementById("result_items").textContent = "are you dumb?";
     }
 }
